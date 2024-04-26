5a00b3f9e2240c3fd667368ec47c791327313f8a
Shindame <morales.edromar@gmail.com>
2024-04-26 14:18:03 +0800

    Main Menu Done: Functionalities Is Next To Be Added

---
+++ Assets/Scripts/LandingMenu/UI Game Menu/ButtonFadeInFadeOut.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonFadeInFadeOut : MonoBehaviour
+{
+    public GameObject ToActivate;
+    public GameObject ToDeactivate;
+
+    public Button button;
+
+    void Start()
+    {
+        button.onClick.AddListener(PlayAnimation);
+    }
+
+    void PlayAnimation()
+    {
+        // Start a coroutine to wait for 2 seconds before activating/deactivating GameObjects
+        StartCoroutine(ActivateDeactivateAfterDelay());
+
+        // Perform additional action when the button is clicked
+        Debug.Log("Button clicked!");
+    }
+
+    IEnumerator ActivateDeactivateAfterDelay()
+    {
+        // Wait for 2 seconds
+        yield return new WaitForSeconds(2f);
+
+        ToActivate.SetActive(true);
+
+
+        ToDeactivate.SetActive(false);
+    }
+}
